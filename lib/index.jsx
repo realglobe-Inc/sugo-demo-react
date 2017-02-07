b747f17265cc575cb252d91a72742eeb53e7122c
Taka Okunishi <okunishinishi@gmail.com>
2017-02-07 09:00:47 +0900

    OSS対応

--- lib/index.jsx
+++ lib/index.jsx
@@ -1,9 +1,10 @@
 /**
  * Demo of react projects
  * @module sugo-demo-react
- * @version 1.1.0
+ * @version 1.1.1
  */
 'use strict'
+
 
 import MyComponentStyle from './my_component_style'
 import MyComponent from './my_component'
